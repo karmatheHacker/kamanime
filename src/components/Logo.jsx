772e2ea07d616f195835201e3b0ed353876a1e93
Rabin <karmahacks545@gmail.com>
2025-10-22 20:21:03 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to={"/home"}>
-      <h1 className="gradient-text select-none flex text-xl">kamanime</h1>
+      <h1 className="gradient-text select-none flex text-xl">KAMANIME</h1>
     </Link>
   );
 };
